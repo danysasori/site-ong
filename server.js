c4c2709e15afa4d84c05aa7eb6a2b637c0aa6e55
danysasori <danysasori@icloud.com>
2026-05-08 00:33:45 -0300

    fix server start

--- server.js
+++ server.js
@@ -223,3 +223,10 @@ app.get('/admin', (req, res) => {
   `);
 
 });
+// ================= SERVIDOR =================
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
